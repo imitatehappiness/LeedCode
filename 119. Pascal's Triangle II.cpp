6602abe2dcdccee416fe275307f9805f008b2d62
IVAN KALENSKY <79199956+imitatehappiness@users.noreply.github.com>
2022-06-28 23:30:33 +0300

    Update 119. Pascal's Triangle II.cpp

--- 119. Pascal's Triangle II.cpp
+++ 119. Pascal's Triangle II.cpp
@@ -8,26 +8,26 @@ using namespace std;
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-            vector<vector<int>> res;
-    vector<int> first{1};
-    if(rowIndex == 0){
-        return first;
-    }
-    vector<int> second{1, 1};
-    if(rowIndex == 1){
-        return second;
-    }
-    res.push_back(second);
-    res.push_back(first);
-
-    for(int i = 2; i < rowIndex + 2; i++){
-        vector<int> tmp{1};
-        for(int j = 0; j < res[i - 1].size() - 1; j++){
-            tmp.push_back(res[i - 1][j] + res[i - 1][j + 1]);
+        vector<vector<int>> res;
+        vector<int> first{1};
+        if(rowIndex == 0){
+            return first;
         }
-        tmp.push_back(1);
-        res.push_back(tmp);
-    }
-    return res[rowIndex + 1];
+        vector<int> second{1, 1};
+        if(rowIndex == 1){
+            return second;
+        }
+        res.push_back(second);
+        res.push_back(first);
+        
+        for(int i = 2; i < rowIndex + 2; i++){
+            vector<int> tmp{1};
+            for(int j = 0; j < res[i - 1].size() - 1; j++){
+                tmp.push_back(res[i - 1][j] + res[i - 1][j + 1]);
+            }
+            tmp.push_back(1);
+            res.push_back(tmp);
+        }
+        return res[rowIndex + 1];
     }
 };
